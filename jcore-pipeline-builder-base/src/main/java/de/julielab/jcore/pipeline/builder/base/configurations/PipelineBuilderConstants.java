4862b39b4afae1c5992501c88c3b995c2fe0e71f
khituras <chew@gmx.net>
2021-06-08 11:18:27 +0200

    Removed unused constants.

--- jcore-pipeline-builder-base/src/main/java/de/julielab/jcore/pipeline/builder/base/configurations/PipelineBuilderConstants.java
+++ jcore-pipeline-builder-base/src/main/java/de/julielab/jcore/pipeline/builder/base/configurations/PipelineBuilderConstants.java
@@ -21,7 +21,6 @@ public class PipelineBuilderConstants {
         public static final String API_REPO = "repos";
         public static final String API_CONTENT = "contents";
         public static final String RAW = "raw.githubusercontent.com";
-        public static final String USER = "JULIELab";
         public static final String API_TYPE = "type";
         public static final String API_FOLDER = "dir";
         public static final String API_PATH = "path";
@@ -37,18 +36,10 @@ public class PipelineBuilderConstants {
                 .add(".jcore-pipeline-builder").toString();
         public static final String REPOSITORIES = "repositories.json";
         public static final String FILE = "component.meta";
-        public static final String EXPOSABLE = "exposable";
-        public static final String MAVEN_ARTIFACT = "maven-artifact";
-        public static final String DESCRIPTOR_LOCATION = "descriptor-location";
-        public static final String NAME = "name";
-        public static final String GROUP = "group";
-        public static final String CATEGORY = "category";
         public static final String CATEGORY_AE = "ae";
         public static final String CATEGORY_CONSUMER = "consumer";
         public static final String CATEGORY_READER = "reader";
         public static final String CATEGORY_MULTIPLIER = "multiplier";
-        public static final String MODULE = "module";
-        public static final String BASE = "base";
 
         public enum Category {reader, ae, consumer, multiplier, flowcontroller}
     }
@@ -58,10 +49,6 @@ public class PipelineBuilderConstants {
      */
     public static final class Maven {
         public static final String LOCAL_REPO = new StringJoiner(File.separator).add(".m2").add("repository").toString();
-        public static final String SONATYPE_REPO_ID = "sonatype-nexus";
-        public static final String SONATYPE_REPO_TYPE = "default";
-        public static final String SONATYPE_REPO_URL = "https://oss.sonatype.org/content/repositories/releases/";
-        public static final String SONATYPE_SNAPSHOT_REPO_URL = "https://oss.sonatype.org/content/repositories/snapshots/";
         public static final CharSequence POM = "pom.xml";
     }
 
@@ -72,12 +59,5 @@ public class PipelineBuilderConstants {
         public static final String CAPABILITIES = "capabilities";
         public static final String CAPABILITIES_IN = "in";
         public static final String CAPABILITIES_OUT = "out";
-    }
-
-    public static final class UIMA {
-        public static final String TYPE_STRING = "String";
-        public static final String TYPE_BOOL = "Boolean";
-        public static final String TYPE_INT = "Integer";
-        public static final String TYPE_FLOAT = "Float";
     }
 }
